1de62c050c21caf4534ce696fcd31f0e5c42711d
Laís Nayra <lais.nayra@gmail.com>
2020-08-08 11:15:00 -0300

    Implementando Exception

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -8,15 +8,21 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TabuleiroXadrez tab = new TabuleiroXadrez(8, 8);
 
-            TabuleiroXadrez tab = new TabuleiroXadrez(8, 8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.ImprimirTabuleiro(tab);
 
-            Tela.ImprimirTabuleiro(tab);
-
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
