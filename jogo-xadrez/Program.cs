c512b6353ab928dfa0a2cd9430d40601e95c720b
Laís Nayra <lais.nayra@gmail.com>
2020-08-08 14:09:34 -0300

    Melhorando método Imprimir

--- jogo-xadrez/Program.cs
+++ jogo-xadrez/Program.cs
@@ -8,11 +8,22 @@ namespace jogo_xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            try
+            {
+                TabuleiroXadrez tab = new TabuleiroXadrez(8, 8);
 
-            Console.WriteLine(pos);
-            Console.WriteLine( pos.StringToPosicao());
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
+                Tela.ImprimirTabuleiro(tab);
+
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
